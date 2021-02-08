d755a4fb97a85060933566e25dc72c352e9a8be0
unknown <alao@lorma.edu>
2021-02-08 18:48:34 +0800

    first commit

---
+++ src/components/Department.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="h-screen w-screen bg-gray-100  flex flex-col ">
+   
+
+     <div
+      class="bg-white border-black rounded shadow  w-1/2 "
+    >
+     
+        <div class="p-8 flex flex-wrap justify-start">
+          <label for="name" class=" w-full  text-left">Department</label>
+          <input type="text" id="name" class="p-2 w-full border-2" />
+        </div>
+        <div
+          class="px-8 py-4 bg-gray-100 border-t border-gray-200 flex justify-end"
+        >
+          <button class="bg-indigo-800 text-white p-2 rounded-md" type="submit">
+            Create Department
+          </button>
+        </div>
+    
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sending: false,
+      form: {
+        name: null,
+      },
+    };
+  },
+};
+</script>
+
+<style>
+</style>
